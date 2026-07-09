700925a6197a9e9a21a0b1cfe9dab9de5d00efcf
Renan Malveira Rodrigues <renan@suricatusgames.com>
2026-07-09 11:49:52 -0300

    Initial commit - QRCode Quiz

---
+++ Quiz - Suricatus/Assets/Scripts/UI/HintScreenController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Core;
+using Data;
+using TMPro;
+using UnityEngine;
+
+namespace UI
+{
+    public class HintScreenController : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI hintText;
+
+        private void OnEnable()
+        {
+            GameManager.OnHintRequested += Populate;
+        }
+
+        private void OnDisable()
+        {
+            GameManager.OnHintRequested -= Populate;
+        }
+
+        private void Populate(StationData data)
+        {
+            hintText.text = data.hintText;
+        }
+
+        public void OnRestartClicked()
+        {
+            GameManager.Instance.Restart();
+        }
+    }
+}
---
+++ Quiz - Suricatus/Assets/Scripts/UI/UIController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+
+namespace UI
+{
+    public enum GameScreen
+    {
+        Quiz,
+        Correct,
+        Hint,
+        Wrong,
+        Final
+    }
+    
+    public class UIController : MonoBehaviour
+    {
+        public static UIController Instance {get; private set;}
+        
+        [Header("Screens")]
+        [SerializeField] private GameObject quizScreen;
+        [SerializeField] private GameObject correctScreen;
+        [SerializeField] private GameObject hintScreen;
+        [SerializeField] private GameObject wrongScreen;
+        [SerializeField] private GameObject finalScreen;
+
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(this.gameObject);
+                return;
+            }
+
+            Instance = this;
+        }
+
+        public void ShowScreen(GameScreen screen)
+        {
+            quizScreen.SetActive(screen == GameScreen.Quiz);
+            correctScreen.SetActive(screen == GameScreen.Correct);
+            hintScreen.SetActive(screen == GameScreen.Hint);
+            wrongScreen.SetActive(screen == GameScreen.Wrong);
+            finalScreen.SetActive(screen == GameScreen.Final);
+        }
+    }
+}
